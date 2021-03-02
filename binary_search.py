6647a97dc25efab459442f2ad4db3731deb9c034
emmacgodfrey <emma.godfrey@pomona.edu>
2021-03-01 18:18:37 -0800

    hello

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 '''
-JOKE: There are 2 hard problems in computer science: cache invalidation,
+joke: There are 2 hard problems in computer science: cache invalidation,
 naming things, and off-by-1 errors.
 
 It's really easy to have off-by-1 errors in these problems.
